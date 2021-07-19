90ba1460185e22a801ff6410c2ae85f8ed80ff7f
GitHub Action <action@github.com>
2021-07-19 03:54:48 +0000

    Template cleanup

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -93,7 +93,7 @@ afterEvaluate {
                         version = "$version-SNAPSHOT"
                     }
                     description.set("A template for Kotlin Android projects")
-                    url.set("https://github.com/cortinico/kotlin-android-template/")
+                    url.set("https://github.com/computr0n/app/")
 
                     licenses {
                         license {
@@ -103,18 +103,18 @@ afterEvaluate {
                     }
                     developers {
                         developer {
-                            id.set("cortinico")
-                            name.set("Nicola Corti")
+                            id.set("computr0n")
+                            name.set("computr0n")
                         }
                     }
                     scm {
-                        connection.set("scm:git:git://github.com/cortinico/kotlin-android-template.git")
-                        developerConnection.set("scm:git:ssh://github.com/cortinico/kotlin-android-template.git")
-                        url.set("https://github.com/cortinico/kotlin-android-template/")
+                        connection.set("scm:git:git://github.com/computr0n/app.git")
+                        developerConnection.set("scm:git:ssh://github.com/computr0n/app.git")
+                        url.set("https://github.com/computr0n/app/")
                     }
                     issueManagement {
                         system.set("GitHub Issues")
-                        url.set("https://github.com/cortinico/kotlin-android-template/issues")
+                        url.set("https://github.com/computr0n/app/issues")
                     }
                 }
             }
